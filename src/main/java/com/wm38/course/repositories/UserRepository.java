5478340fbb524eec5146261416319b26289ff0d5
Wagner1218 <wagnermarin38@gmail.com>
2025-03-01 22:31:48 -0300

    Service layer, component registration

--- src/main/java/com/wm38/course/repositories/UserRepository.java
+++ src/main/java/com/wm38/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wm38.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	
---
+++ src/main/java/com/wm38/course/services/UserSerice.java
@@ -0,0 +1,28 @@
+package com.wm38.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wm38.course.entities.User;
+import com.wm38.course.repositories.UserRepository;
+
+@Service
+public class UserSerice {
+
+	//Informa que o userService depende do UserRepository
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
